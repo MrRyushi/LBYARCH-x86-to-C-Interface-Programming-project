79b21c4a781fb0c03ba969bb58a4068c65cf6054
MrRyushi <john_patrick_marcellana@dlsu.edu.ph>
2024-03-27 21:17:45 +0800

    tried to pass vector

--- x86_64_MP/asmfunc.asm
+++ x86_64_MP/asmfunc.asm
@@ -1,18 +1,27 @@
 ; assembly part using x86-64
-
 section .data
-msg db "Hello John", 13, 10, 0
+length dq 0.0
 
 section .text
 bits 64
 default rel ; to handle address releocation
 
-global asmhello
+global dotproduct
 extern printf
 
-asmhello:
-	sub rsp, 8*5 ; caller
-	lea rcx, [msg]
+dotproduct:
+sub rsp, 8*7
+	mov rax, [var6]
+	mov [rsp+48],rax ; 7th parameter
+	mov rax, [var5]
+	mov [rsp+40],rax ; 6th parameter
+	mov rax, [var4]
+	mov [rsp+32],rax ; 5th parameter
+	mov r9, [var3] ; 4th parameter
+	mov r8, [var2] ; 3rd parameter
+	mov rdx, [var1] ; 2nd parameter
+	mov rcx, msg1 ; 1st parameter
 	call printf
-	add rsp, 8*5
-	ret
+add rsp, 8*7
+
+ret
--- x86_64_MP/main.c
+++ x86_64_MP/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
 
-extern void asmhello();
+extern float dotproduct(float *vectorA, float *vectorB, int length);
+
 int main() {
-	asmhello();
-	return 0;
+    // Seed the random number generator
+    srand((unsigned int)time(NULL));
+
+    // get the length of vectors
+    printf("Input Vector Length: ");
+    int length = 0;
+    scanf_s("%d", &length);
+
+    // declare the vectors and sdot variable
+    float* vectorA = malloc(length * sizeof(float));
+    float* vectorB = malloc(length * sizeof(float));
+    float sdot = 0;
+
+    // Initialize random data
+    for (int i = 0; i < length; i++) {
+        // Generate a random integer between 1 and 1000 (for more precision)
+        int randomInt = rand() % 1000 + 1;
+        // Convert to float and scale to desired range (1 to 10)
+        float randomValue = (float)randomInt / 100.0f;
+        vectorA[i] = randomValue;
+        vectorB[i] = randomValue;
+        printf("value %i = %f \n", i, randomValue);
+    }
+
+    printf("%f", dotproduct(vectorA, vectorB, length));
+    printf("sdot = %f \n", sdot);
+
+    // Don't forget to free allocated memory when done
+    free(vectorA);
+    free(vectorB);
+
+    return 0;
+
 }
